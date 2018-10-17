7ab4b6349d9dadbb126d84cd4a6c970df4eeb8dc
ting102803 <ting102803@naver.com>
2018-10-17 16:42:38 +0900

    Java 8kyu

---
+++ Simple Fun #1 Seats in Theater.java
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/588417e576933b0ec9000045
+
+public class Kata {
+
+  public static int seatsInTheater(int nCols, int nRows, int col, int row) {
+  
+    return (nCols-col+1)*(nRows-row);
+    
+  }
+
+}
